cd612fd0e15d0bb46f7078ccbe325f33b5467471
arXaik <113052184+arXaik@users.noreply.github.com>
2022-09-15 01:21:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//Петросян вариант 16
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+double S = Math.Sqrt(Math.Cos(4 * Math.Pow(y,2))+ 7.151 );
+Console.WriteLine($"S={S:F2}");
